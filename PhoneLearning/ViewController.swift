3d916698f4f0db766128902cb4ef49e76a4836aa
Free Debreuil <freedebreuil@gmail.com>
2016-04-20 20:24:44 +0900

    first compiling to device working

--- PhoneLearning/ViewController.swift
+++ PhoneLearning/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController
         // Do any additional setup after loading the view, typically from a nib.
         
         initMetal()
-        initGame()
+//        initGame()
     }
     func initGame()
     {
@@ -31,21 +31,21 @@ class ViewController: UIViewController
         
         // prepare sigmoid function to run on gpu
         // a. initialize Metal
-        var (device, commandQueue, defaultLibrary, commandBuffer, computeCommandEncoder) = initMetal()
+        let (device, commandQueue, defaultLibrary, commandBuffer, computeCommandEncoder) = initMetal()
         
         // b. set up a compute pipeline with Sigmoid function and add it to encoder
         let sigmoidProgram = defaultLibrary.newFunctionWithName("sigmoid")
 //        var pipelineErrors = NSErrorPointer()
 //        var commandBuffer = MTLCommandBuffer();
-        var computePipelineFilter = try? device.newComputePipelineStateWithFunction(sigmoidProgram!)//, completionHandler: rror: pipelineErrors)
+        let computePipelineFilter = try? device.newComputePipelineStateWithFunction(sigmoidProgram!)//, completionHandler: rror: pipelineErrors)
         computeCommandEncoder.setComputePipelineState(computePipelineFilter!)
         
         // create gpu input and output data
         // a. calculate byte length of input data - myvector
-        var myvectorByteLength = myvector.count*sizeofValue(myvector[0])
+        let myvectorByteLength = myvector.count*sizeofValue(myvector[0])
         
         // b. create a MTLBuffer - input data that the GPU and Metal and produce
-        var inVectorBuffer = device.newBufferWithBytes(&myvector, length: myvectorByteLength, options: MTLResourceOptions.CPUCacheModeDefaultCache)
+        let inVectorBuffer = device.newBufferWithBytes(&myvector, length: myvectorByteLength, options: MTLResourceOptions.CPUCacheModeDefaultCache)
         
         // c. set the input vector for the Sigmoid() function, e.g. inVector
         //    atIndex: 0 here corresponds to buffer(0) in the Sigmoid function
@@ -54,34 +54,38 @@ class ViewController: UIViewController
         // d. create the output vector for the Sigmoid() function, e.g. outVector
         //    atIndex: 1 here corresponds to buffer(1) in the Sigmoid function
         var resultdata = [Float](count:myvector.count, repeatedValue: 0)
-        var outVectorBuffer = device.newBufferWithBytes(&resultdata, length: myvectorByteLength, options: MTLResourceOptions.CPUCacheModeDefaultCache)
+        let outVectorBuffer = device.newBufferWithBytes(&resultdata, length: myvectorByteLength, options: MTLResourceOptions.CPUCacheModeDefaultCache)
         computeCommandEncoder.setBuffer(outVectorBuffer, offset: 0, atIndex: 1)
+        
+        // end encoding for the compute command buffer
+        computeCommandEncoder.endEncoding();
         
         // configure gpu threads
         // hardcoded to 32 for now (recommendation: read about threadExecutionWidth)
-        var threadsPerGroup = MTLSize(width:32,height:1,depth:1)
-        var numThreadgroups = MTLSize(width:(myvector.count+31)/32, height:1, depth:1)
+        let threadsPerGroup = MTLSize(width:32,height:1,depth:1)
+        let numThreadgroups = MTLSize(width:(myvector.count+31)/32, height:1, depth:1)
         computeCommandEncoder.dispatchThreadgroups(numThreadgroups, threadsPerThreadgroup: threadsPerGroup)
     }
     func initMetal() -> (MTLDevice, MTLCommandQueue, MTLLibrary, MTLCommandBuffer,
         MTLComputeCommandEncoder)
     {
-            // Get access to iPhone or iPad GPU
-            let device = MTLCreateSystemDefaultDevice()
+        // Get access to iPhone or iPad GPU
+        let device = MTLCreateSystemDefaultDevice()
             
-            // Queue to handle an ordered list of command buffers
-            let commandQueue = device!.newCommandQueue()
+        // Queue to handle an ordered list of command buffers
+        let commandQueue = device!.newCommandQueue()
             
-            // Access to Metal functions that are stored in Shaders.metal file, e.g. sigmoid()
-            let defaultLibrary = device!.newDefaultLibrary()
+        // Access to Metal functions that are stored in Shaders.metal file, e.g. sigmoid()
+        let defaultLibrary = device!.newDefaultLibrary()
             
-            // Buffer for storing encoded commands that are sent to GPU
-            let commandBuffer = commandQueue.commandBuffer()
+        // Buffer for storing encoded commands that are sent to GPU
+        let commandBuffer = commandQueue.commandBuffer()
             
-            // Encoder for GPU commands
-            let computeCommandEncoder = commandBuffer.computeCommandEncoder()
+        // Encoder for GPU commands
+        let computeCommandEncoder = commandBuffer.computeCommandEncoder()
+        computeCommandEncoder.endEncoding();
             
-            return (device!, commandQueue, defaultLibrary!, commandBuffer, computeCommandEncoder)
+        return (device!, commandQueue, defaultLibrary!, commandBuffer, computeCommandEncoder)
     }
 
 
